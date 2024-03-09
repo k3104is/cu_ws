8d0fe1ee70a945aff9523a15c5f2475a9914f649
Satoshi Kataoka <k.3104is@gmail.com>
2024-03-09 18:38:53 +0900

    add comments

--- workdir/cu_100s/app.cu
+++ workdir/cu_100s/app.cu
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
+/* CUDA KERNEL */
 __global__ void add(int* a, int* b, int* c)
 {
   int i = threadIdx.x + blockDim.x * blockIdx.x;
   c[i] = a[i] + b[i];
 }
 
+/* MEMORY SPACE SPECIFIER */
 __managed__ int vector_a[256], vector_b[256], vector_c[256];
 
 int main()
 {
+  int result_sum = 0;
+
   for (int i = 0; i < 256; i++)
   {
     vector_a[i] = i;
     vector_b[i] = 256 - i;
 
+    /* LAUNCH KERNEL */
+    /* <<<blocks, threads per block>>> */
     add<<<1, 256>>>(vector_a, vector_b, vector_c);
+
+    /* WAIT FOR GPU */
     cudaDeviceSynchronize();
 
-    int result_sum = 0;
-
-    for (int i = 0; i < 256; i++)
-    {
-      result_sum += vector_c[i];
-    }
-    printf("result_sum:%d\n", result_sum);
   }
+  for (int i = 0; i < 256; i++)
+  {
+    result_sum += vector_c[i];
+  }
+  printf("result_sum:%d\n", result_sum);
   return 0;
 }
